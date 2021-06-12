628ccf3df2db66c5536c24eb0243cf3c234ca6e3
doylejordyn <doylejordyn@gmail.com>
2021-06-11 22:31:21 -0400

    1

---
+++ js/index.js
@@ -0,0 +1,223 @@
+console.log("index.js is loaded correctly.")
+
+$(".navlinkone").mouseover(function(){
+    $(".navlinkone").css("color", "#5B04CF");
+  });
+
+  $(".navlinktwo").mouseover(function(){
+    $(".navlinktwo").css("color", "#5B04CF");
+  });
+
+$(".navlinkthree").mouseover(function(){
+    $(".navlinkthree").css("color", "#5B04CF");
+  });
+
+$(".navlinkfour").mouseover(function(){
+    $(".navlinkfour").css("color", "#5B04CF");
+  });
+
+
+  $(".navlinkone").mouseleave(function(){
+    $(".navlinkone").css("color", "white");
+  });
+
+  $(".navlinktwo").mouseleave(function(){
+    $(".navlinktwo").css("color", "white");
+  });
+
+  $(".navlinkthree").mouseleave(function(){
+    $(".navlinkthree").css("color", "white");
+  });
+
+  $(".navlinkfour").mouseleave(function(){
+    $(".navlinkfour").css("color", "white");
+  });
+
+
+$("#gridimageone").css("opacity","1.0");
+
+$("#gridimageone").hover(function () {
+
+$(this).stop().animate({
+opacity: 0.5
+}, "slow");
+},
+
+function () {
+    $(this).stop().animate({
+        opacity: 1.0
+    }, "slow");
+});
+
+
+
+$("#gridimagetwo").css("opacity","1.0");
+
+$("#gridimagetwo").hover(function () {
+
+$(this).stop().animate({
+opacity: 0.5
+}, "slow");
+},
+
+
+function () {
+
+
+$(this).stop().animate({
+opacity: 1.0
+}, "slow");
+});
+
+
+$("#gridimagethree").css("opacity","1.0");
+
+
+$("#gridimagethree").hover(function () {
+
+
+$(this).stop().animate({
+opacity: 0.5
+}, "slow");
+},
+
+
+function () {
+
+
+$(this).stop().animate({
+opacity: 1.0
+}, "slow");
+});
+
+
+
+$("#gridimagefour").css("opacity","1.0");
+
+
+$("#gridimagefour").hover(function () {
+
+
+$(this).stop().animate({
+opacity: 0.5
+}, "slow");
+},
+
+
+function () {
+
+
+$(this).stop().animate({
+opacity: 1.0
+}, "slow");
+});
+
+
+
+$("#gridimagefive").css("opacity","1.0");
+
+
+$("#gridimagefive").hover(function () {
+
+
+$(this).stop().animate({
+opacity: 0.5
+}, "slow");
+},
+
+
+function () {
+
+
+$(this).stop().animate({
+opacity: 1.0
+}, "slow");
+});
+
+
+
+$("#gridimagesix").css("opacity","1.0");
+
+
+$("#gridimagesix").hover(function () {
+
+
+$(this).stop().animate({
+opacity: 0.5
+}, "slow");
+},
+
+
+function () {
+
+
+$(this).stop().animate({
+opacity: 1.0
+}, "slow");
+});
+
+
+
+$("#gridimageseven").css("opacity","1.0");
+
+
+$("#gridimageseven").hover(function () {
+
+
+$(this).stop().animate({
+opacity: 0.5
+}, "slow");
+},
+
+
+function () {
+
+
+$(this).stop().animate({
+opacity: 1.0
+}, "slow");
+});
+
+
+
+$("#gridimageeight").css("opacity","1.0");
+
+
+$("#gridimageeight").hover(function () {
+
+
+$(this).stop().animate({
+opacity: 0.5
+}, "slow");
+},
+
+
+function () {
+
+
+$(this).stop().animate({
+opacity: 1.0
+}, "slow");
+});
+
+
+
+$("#gridimagenine").css("opacity","1.0");
+
+
+$("#gridimagenine").hover(function () {
+
+
+$(this).stop().animate({
+opacity: 0.5
+}, "slow");
+},
+
+
+function () {
+
+
+$(this).stop().animate({
+opacity: 1.0
+}, "slow");
+});
